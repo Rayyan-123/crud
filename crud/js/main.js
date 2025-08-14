865b4a4d84c6fc8c479e5870498478943307b9a2
Rayyan-123 <161798434+Rayyan-123@users.noreply.github.com>
2025-08-14 23:31:29 +0500

    Delete crud directory

--- crud/js/main.js
+++
@@ -1,6 +0,0 @@
-import { ProductManager } from './modules/ProductManager.js';
-import { UI } from './modules/UI.js';
-
-// Initialize the application
-const productManager = new ProductManager();
-const ui = new UI(productManager);
